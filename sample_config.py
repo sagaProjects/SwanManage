6fbd287e30917993eb9451132d021a0125af10ea
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2022-11-14 12:06:48 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "5764925353:AAF4tkG4yDutLRmhDZdDFuZcWuvRY50soOA"
 API_ID = 13515342
-SESSION_STRING = "" # Check Readme to generate sessions; need for userbot
+SESSION_STRING = "BQA3QOQtk1aZYOBjK4ud5mf37pCNiniluzhnScB0mP4HBiKidxyUkGngy72cV6U2te6BL_o1bnDG_xUf04z_SzKNm89FCQZrLFJMIboMVPrEP_CpP9g1XPjeEJh1F1xbuR1UFNPvsJjh5qXyYxpVYAmVY81RRoW1ahq8G5qG1iGxeYDY3yBpcdpI82w62_eXINrZ0nf7K0bjoeCttv5vIqshK7I3LomY0GCtMEYMJYtTEfnaTRon4WAuRCEKS0JVoZiZig-ahBqH2xqX1qfgav4XoMN2PzogmiXD7PY2aymIb-OvSLpHXImJIzst1pfWpCNA9gqqTStfct4xiJcOHjaBeuNxRAA" # Check Readme to generate sessions; need for userbot
 API_HASH = "8cb1a48861ddd8063adb972a44b2b1f6"
 USERBOT_PREFIX = "/"
 PHONE_NUMBER = "+6285697204981"  # Need for Userbot
