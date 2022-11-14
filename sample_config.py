4b6f69d3baa64ccf28420c8a8921e6438422375d
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2022-11-14 12:04:48 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,19 +1,19 @@
-BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
-API_ID = 123456
+BOT_TOKEN = "5764925353:AAF4tkG4yDutLRmhDZdDFuZcWuvRY50soOA"
+API_ID = 13515342
 SESSION_STRING = "" # Check Readme to generate sessions; need for userbot
-API_HASH = "dfxcgs5s12hdcxfgdfz"
-USERBOT_PREFIX = "."
-PHONE_NUMBER = "+916969696969"  # Need for Userbot
+API_HASH = "8cb1a48861ddd8063adb972a44b2b1f6"
+USERBOT_PREFIX = "/"
+PHONE_NUMBER = "+6285697204981"  # Need for Userbot
 SUDO_USERS_ID = [
-    4543744343,
-    543214651351,
+    1293361587,
+    1385170049,
 ]  # Sudo users have full access to everything, don't trust anyone
-LOG_GROUP_ID = -100125431255
-GBAN_LOG_GROUP_ID = -100125431255
-MESSAGE_DUMP_CHAT = -1001181696437
+LOG_GROUP_ID = -1001896097424
+GBAN_LOG_GROUP_ID = -1001896097424
+MESSAGE_DUMP_CHAT = -1001896097424
 WELCOME_DELAY_KICK_SEC = 300
-MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
-ARQ_API_KEY = "Get this from @ARQRobot"
+MONGO_URL = "mongodb+srv://saga:saga@cluster0.bdv3ezf.mongodb.net/?retryWrites=true&w=majority"
+ARQ_API_KEY = "KXJZMV-APXUVH-IAKLMF-BUCFAA-ARQ"
 ARQ_API_URL = "https://arq.hamker.in"
 LOG_MENTIONS = True
 RSS_DELAY = 300  # In seconds
