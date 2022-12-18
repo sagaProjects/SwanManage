988b1aac9726e3d8d3bf278253df03f662f30028
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2022-12-18 16:13:26 +0700

    Update tagall.py

--- wbb/modules/tagall.py
+++ wbb/modules/tagall.py
@@ -1,7 +1,7 @@
 from asyncio import sleep
 from pyrogram import filters
 from wbb import app
-from wbb.core.decorators.permissions import adminsOnly
+from wbb.core.decorators.permissions import memberOnly
 
 __MODULE__ = "Tagall"
 __HELP__ = """/all - mention all members
@@ -11,7 +11,7 @@ __HELP__ = """/all - mention all members
 spam_chats = []
 
 @app.on_message(filters.command("all") & filters.group)
-@adminsOnly("")
+@memberOnly("")
 async def mentionall(client, message):
     await message.delete()
     chat_id = message.chat.id
